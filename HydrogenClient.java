f2d0bb7f69c14d40ead3300fa5abce5904ddb46e
Kyle Martinez <kyle.k.martinez@gmail.com>
2024-03-08 13:41:15 +0800

    Added Logs and basic requests

--- HydrogenClient.java
+++ HydrogenClient.java
@@ -1,10 +1,30 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class HydrogenClient {
+    private static int nH;
+    private static ArrayList<String> HydrogenClientLogs = new ArrayList<>();
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of hydrogen atoms:");
-        int nH = scanner.nextInt();
+        nH = scanner.nextInt();
         scanner.close();
+
+        createRequests();
+    }
+    private static void createRequests() {
+        for (int i = 0; i < nH; i++) {
+            String id = "H" + i;
+            String log = id + ", request, " + sdf.format(new Date());
+            System.err.println(log);
+            HydrogenClientLogs.add(log);
+            sendRequest(id);
+        }
+    }
+    private static void sendRequest(String id) {
+        // TODO: Add code to send request to server
     }
 }
--- OxygenClient.java
+++ OxygenClient.java
@@ -1,10 +1,31 @@
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class OxygenClient {
+    private static int nO;
+    private static ArrayList<String> OxygenClientLogs = new ArrayList<>();
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of oxygen atoms:");
-        int nO = scanner.nextInt();
+        nO = scanner.nextInt();
         scanner.close();
+
+        createRequests();
+    }
+
+    private static void createRequests() {
+        for (int i = 0; i < nO; i++) {
+            String id = "O" + i;
+            String log = id + ", request, " + sdf.format(new Date());
+            OxygenClientLogs.add(log);
+            sendRequest(id);
+        }
+    }
+
+    private static void sendRequest(String id) {
+        // TODO: Add code to send request to server
     }
 }
