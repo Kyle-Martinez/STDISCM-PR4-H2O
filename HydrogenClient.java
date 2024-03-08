92b4ab0f0580ee2e6c608a73fca1b82023105744
Kyle Martinez <kyle.k.martinez@gmail.com>
2024-03-08 13:28:16 +0800

    Added Scanners

--- HydrogenClient.java
+++ HydrogenClient.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HydrogenClient {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of hydrogen atoms:");
+        int nH = scanner.nextInt();
+        scanner.close();
     }
 }
--- OxygenClient.java
+++ OxygenClient.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class OxygenClient {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of oxygen atoms:");
+        int nO = scanner.nextInt();
+        scanner.close();
     }
 }
